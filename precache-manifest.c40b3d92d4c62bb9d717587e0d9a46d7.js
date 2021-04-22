3961900facedffeb3d6ceb4ef93cf92a6f7cb2a0
Tim McMackin <timothymcmackin@yahoo.com>
2021-04-21 21:18:23 -0400

    Updates

---
+++ precache-manifest.c40b3d92d4c62bb9d717587e0d9a46d7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe80704a3ee9b697ffbd",
+    "url": "/static.kitepaint/static/css/main.07a7d827.chunk.css"
+  },
+  {
+    "revision": "fe80704a3ee9b697ffbd",
+    "url": "/static.kitepaint/static/js/main.fe80704a.chunk.js"
+  },
+  {
+    "revision": "adb6acec2b3f34e64de0",
+    "url": "/static.kitepaint/static/css/1.a9b94598.chunk.css"
+  },
+  {
+    "revision": "adb6acec2b3f34e64de0",
+    "url": "/static.kitepaint/static/js/1.adb6acec.chunk.js"
+  },
+  {
+    "revision": "ba80984c3f3f42fdef9c",
+    "url": "/static.kitepaint/static/js/runtime~main.ba80984c.js"
+  },
+  {
+    "revision": "f5e486e30aabeae42551c53aa5b904fd",
+    "url": "/static.kitepaint/static/media/bg-rev.f5e486e3.jpg"
+  },
+  {
+    "revision": "ba70883a53e3f6ff24ce4c6bbe14582c",
+    "url": "/static.kitepaint/static/media/dual-line.ba70883a.svg"
+  },
+  {
+    "revision": "e47a29cb99abda1490dd8bd1359b25ae",
+    "url": "/static.kitepaint/static/media/quad-line.e47a29cb.svg"
+  },
+  {
+    "revision": "adae5e6b6b6463b204dfdc99e8f10d33",
+    "url": "/static.kitepaint/index.html"
+  }
+];
