adaf22471d6837b47a82a01d081dd99103886866
Tim McMackin <timothymcmackin@yahoo.com>
2021-05-06 19:20:28 -0400

    Updates

--- precache-manifest.c40b3d92d4c62bb9d717587e0d9a46d7.js
+++ precache-manifest.c40b3d92d4c62bb9d717587e0d9a46d7.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "59d34aaefed2df6e4b44",
+    "revision": "fe80704a3ee9b697ffbd",
     "url": "/static.kitepaint/static/css/main.07a7d827.chunk.css"
   },
   {
-    "revision": "59d34aaefed2df6e4b44",
-    "url": "/static.kitepaint/static/js/main.59d34aae.chunk.js"
+    "revision": "fe80704a3ee9b697ffbd",
+    "url": "/static.kitepaint/static/js/main.fe80704a.chunk.js"
   },
   {
     "revision": "adb6acec2b3f34e64de0",
@@ -32,7 +32,7 @@ self.__precacheManifest = [
     "url": "/static.kitepaint/static/media/quad-line.e47a29cb.svg"
   },
   {
-    "revision": "49ce215e1ee3363eb57e9d04f9a3cf64",
+    "revision": "adae5e6b6b6463b204dfdc99e8f10d33",
     "url": "/static.kitepaint/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/static.kitepaint/precache-manifest.781aaf22fd9cd1659293bb15af5bb3ad.js"
+  "/static.kitepaint/precache-manifest.c40b3d92d4c62bb9d717587e0d9a46d7.js"
 );
 
 workbox.clientsClaim();
